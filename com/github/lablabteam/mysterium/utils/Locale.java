2b5b22f8699d47bb64a767bc6a2e302974570425
DevPGSV <DevPGSV@gmail.com>
2016-03-04 21:36:04 +0100

    Added Locale singleton

---
+++ com/github/lablabteam/mysterium/utils/Locale.java
@@ -0,0 +1,36 @@
+package com.github.lablabteam.mysterium.utils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
+
+@SuppressWarnings("rawtypes")
+public class Locale {
+    private static Map currentLocale;
+    private static Map allLocale;
+    private static Locale Instance;
+    
+    private Locale() throws FileNotFoundException, YamlException {
+        YamlReader reader = new YamlReader(new FileReader("res/locale/default.yml"));
+        allLocale = (Map) reader.read();
+        setLocale("default");
+    }
+    
+    public static Locale getInstance() throws FileNotFoundException, YamlException {
+        if (Locale.Instance == null) {
+            Locale.Instance = new Locale();
+        }
+        return Locale.Instance;
+    }
+    
+    public void setLocale(String language) {
+        currentLocale = (Map) allLocale.get(language);
+    }
+    
+    public String getText(String msgKey) {
+        return currentLocale.get(msgKey).toString();
+    }
+}
