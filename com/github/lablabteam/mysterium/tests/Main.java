9407dbb774bf056958d48995e5a4f8246f48bdd6
DevPGSV <DevPGSV@gmail.com>
2016-03-05 15:03:51 +0100

    Locale loaded in main

--- com/github/lablabteam/mysterium/tests/Main.java
+++ com/github/lablabteam/mysterium/tests/Main.java
@@ -3,6 +3,7 @@ package com.github.lablabteam.mysterium.tests;
 import java.io.IOException;
 import com.github.lablabteam.mysterium.db.YamlDB;
 import com.github.lablabteam.mysterium.logic.FSM;
+import com.github.lablabteam.mysterium.utils.locale.Locale;
 import com.github.lablabteam.mysterium.view.GuiView;
 import com.github.lablabteam.mysterium.view.TermView;
 
@@ -10,6 +11,8 @@ import com.github.lablabteam.mysterium.view.TermView;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Locale.getInstance();
+        
         FSM f = new FSM();
         f.runStateMachine();
     }
