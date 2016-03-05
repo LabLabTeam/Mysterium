83548733ef58b92d0660927d64c71d1ea7ac7926
DevPGSV <DevPGSV@gmail.com>
2016-03-05 13:07:20 +0100

    Updated main to create a FSM and run it

--- com/github/lablabteam/mysterium/tests/Main.java
+++ com/github/lablabteam/mysterium/tests/Main.java
@@ -1,17 +1,17 @@
 package com.github.lablabteam.mysterium.tests;
 
 import java.io.IOException;
-
 import com.github.lablabteam.mysterium.db.YamlDB;
+import com.github.lablabteam.mysterium.logic.FSM;
+import com.github.lablabteam.mysterium.view.GuiView;
+import com.github.lablabteam.mysterium.view.TermView;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        
-        YamlDB db = new YamlDB();
-        db.p();
-        db.writeToFile();
+        FSM f = new FSM();
+        f.runStateMachine();
     }
 
 }
