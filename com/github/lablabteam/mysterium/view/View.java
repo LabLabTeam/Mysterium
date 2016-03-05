80c1f4eb355f114c4a8cf38b5bbe8da1b921f970
DevPGSV <DevPGSV@gmail.com>
2016-03-05 13:02:18 +0100

    Added constructor and displayLoading methods to abstract class View

--- com/github/lablabteam/mysterium/view/View.java
+++ com/github/lablabteam/mysterium/view/View.java
@@ -1,6 +1,16 @@
 package com.github.lablabteam.mysterium.view;
 
+import java.util.List;
+
+import com.github.lablabteam.mysterium.logic.Controller;
+import com.github.lablabteam.mysterium.utils.CallbackOnFinished;
 
 public abstract class View {
+    protected Controller controller;
     
+    public View(Controller controller) {
+        this.controller = controller;
+    }
+    
+    public abstract <T> void displayLoading(T obj, CallbackOnFinished<T> cb, List<String> paths);
 }
